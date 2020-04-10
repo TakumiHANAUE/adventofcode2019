f26f9274907b437352eeed74a50c4df8a5903f07
TakumiHANAUE <tkmhana@gmail.com>
2020-04-10 21:57:08 +0900

    day7 part2 program

--- day7/amplifier.nim
+++ day7/amplifier.nim
@@ -1,4 +1,105 @@
-
+import deques
+import opecode
+import strutils
 
 type
     Amplifier* = ref object of RootObj
+        name*: string
+        programSeq: seq[int]
+        programPointer: int
+        phase*: int
+        phaseSettingDone: bool
+        input: Deque[int]
+        output: int
+        isRunnning: bool
+        nextAmp*: Amplifier
+
+method powerOn*(amp: Amplifier): void {.base.} =
+    amp.isRunnning = true
+
+method resetParams*(amp: Amplifier): void {.base.} = 
+    amp.programPointer = 0
+    amp.programSeq = @[]
+    amp.phase = 0
+    amp.phaseSettingDone = false
+    amp.input = initDeque[int]()
+    amp.output = 0
+    amp.isRunnning = false
+
+method setNextAmp*(amp: Amplifier, nextAmplifier: Amplifier): void {.base.} =
+    amp.nextAmp = nextAmplifier
+
+method setPhaseValue*(amp: Amplifier, phaseValue: int): void {.base.} =
+    amp.phase = phaseValue
+
+method addInputValue*(amp: Amplifier, inputValue: int): void {.base.} =
+    amp.input.addLast(inputValue)
+
+method readProgram*(amp: Amplifier, programFilePath: string): void {.base.} =
+    var inputFile: File = open(programFilePath, fmRead)
+    var program: string = inputFile.readAll()
+    close(inputFile)
+    for code in program.split(","):
+        amp.programSeq.add(parseInt(code))
+
+method receiveInput(amp: Amplifier, inputValue: int): void {.base.} =
+    amp.input.addLast(inputValue)
+    # echo amp.name, " : Receive ", inputValue
+    # echo amp.name, " : Input : ", amp.input
+
+method getRunnningState*(amp: Amplifier): bool {.base.} =
+    return amp.isRunnning
+
+method getOutput*(amp: Amplifier): int {.base.} =
+    return amp.output
+
+method giveOutputTo(amp: Amplifier, ampGivenTo: Amplifier, outputValue: int): void {.base.} =
+    if ampGivenTo.getRunnningState == true:
+        # echo amp.name, " : Give output value ", outputValue, " to ", ampGivenTo.name
+        ampGivenTo.receiveInput(outputValue)
+    else:
+        amp.output = outputValue
+        echo amp.name, " : Output ", outputValue
+
+method doOneInstruction*(amp: Amplifier): void {.base.} =
+    if amp.isRunnning == true:
+        var pointerShift: int = 0
+        if getInstCode(amp.programSeq[amp.programPointer]) != 99:
+            case getInstCode(amp.programSeq[amp.programPointer])
+            of 1:
+                opeCode1(amp.programPointer, amp.programSeq)
+                pointerShift = 4
+            of 2:
+                opeCode2(amp.programPointer, amp.programSeq)
+                pointerShift = 4
+            of 3:
+                if ( amp.phaseSettingDone == false ):
+                    opeCode3(amp.programPointer, amp.programSeq, amp.phase)
+                    amp.phaseSettingDone = true
+                    pointerShift = 2
+                else:
+                    # echo amp.name, ": waiting input", " : input.len = ", amp.input.len(), " : ", amp.input
+                    if amp.input.len() != 0:
+                        opeCode3(amp.programPointer, amp.programSeq, amp.input.popFirst())
+                        pointerShift = 2
+            of 4:
+                var outputValue = opeCode4(amp.programPointer, amp.programSeq)
+                amp.giveOutputTo(amp.nextAmp, outputValue)
+                pointerShift = 2
+            of 5:
+                opeCode5(amp.programPointer, amp.programSeq, pointerShift)
+            of 6:
+                opeCode6(amp.programPointer, amp.programSeq, pointerShift)
+            of 7:
+                opeCode7(amp.programPointer, amp.programSeq)
+                pointerShift = 4
+            of 8:
+                opeCode8(amp.programPointer, amp.programSeq)
+                pointerShift = 4
+            else:
+                echo "ERR: Invalid opecode ", amp.programPointer, " ", amp.programSeq[amp.programPointer]
+            inc(amp.programPointer, pointerShift)
+            # echo amp.name, "(", amp.programPointer, ") : ", amp.programSeq
+        else:
+            amp.isRunnning = false
+
--- day7/day7_part2.nim
+++ day7/day7_part2.nim
@@ -1,91 +1,71 @@
 import system
-import strutils
-import opecode
 import algorithm
 import tables
 import sequtils
+import amplifier
+import os
 
 
-var inputFile: File
-var program: string
-var phaseSeq: seq[int] = @[9, 7, 8, 5, 6]
-let amplifiers: seq[string] = @["A", "B", "C", "D", "E"]
-# phaseSettingTable(Amplifier ID, phase setting)
-var phaseSetingTable = initTable[string, int]()
-var nthOpecode3: int = 0
-var input: int = 0
-var output: int = 0
+var ampA: Amplifier = Amplifier(name: "A")
+var ampB: Amplifier = Amplifier(name: "B")
+var ampC: Amplifier = Amplifier(name: "C")
+var ampD: Amplifier = Amplifier(name: "D")
+var ampE: Amplifier = Amplifier(name: "E")
+ampA.setNextAmp(ampB)
+ampB.setNextAmp(ampC)
+ampC.setNextAmp(ampD)
+ampD.setNextAmp(ampE)
+ampE.setNextAmp(ampA)
+
+let programFilePath: string = "./day7input.txt"
+var phaseSeq: seq[int] = @[5, 6, 7, 8, 9]
+let ampNameSeq: seq[string] = @["A", "B", "C", "D", "E"]
+var phaseSettingTable = initTable[string, int]()
 var thrusterSignalSeq: seq[int]
-var phaseSettingDone: Table[string, bool]
-    = {"A": false, "B": false, "C": false, "D": false, "E": false}.toTable
 
 while ( phaseSeq.nextPermutation() == true ):
-    # reset parameters
-    input = 0
-    output = 0
-    for key in phaseSettingDone.keys:
-        phaseSettingDone[key] = false
     # make phaseSettingTable
-    for pairs in zip(amplifiers, phaseSeq):
-        let (amp, phase) = pairs
-        phaseSetingTable[amp] = phase
-    echo phaseSetingTable
+    for pairs in zip(ampNameSeq, phaseSeq):
+        let (name, phase) = pairs
+        phaseSettingTable[name] = phase
+    echo phaseSettingTable
 
-    for loop in 1..10:
-        for key in phaseSettingDone.keys:
-            phaseSettingDone[key] = false
-        
-        for amp in amplifiers:
-            echo amp
-            input = output
-            var programSeq: seq[int] = @[]
-            nthOpecode3 = 0
+    # reset Amplifiers
+    ampA.resetParams()
+    ampB.resetParams()
+    ampC.resetParams()
+    ampD.resetParams()
+    ampE.resetParams()
+    # store input intcode program
+    ampA.readProgram(programFilePath)
+    ampB.readProgram(programFilePath)
+    ampC.readProgram(programFilePath)
+    ampD.readProgram(programFilePath)
+    ampE.readProgram(programFilePath)
+    # set phase value
+    ampA.setPhaseValue(phaseSettingTable["A"])
+    ampB.setPhaseValue(phaseSettingTable["B"])
+    ampC.setPhaseValue(phaseSettingTable["C"])
+    ampD.setPhaseValue(phaseSettingTable["D"])
+    ampE.setPhaseValue(phaseSettingTable["E"])
+    # set input value to first amplifier
+    ampA.addInputValue(0)
+    # power on
+    ampA.powerOn()
+    ampB.powerOn()
+    ampC.powerOn()
+    ampD.powerOn()
+    ampE.powerOn()
+    # run program
+    while ampE.getRunnningState == true:
+        # sleep(500)
+        ampA.doOneInstruction()
+        ampB.doOneInstruction()
+        ampC.doOneInstruction()
+        ampD.doOneInstruction()
+        ampE.doOneInstruction()
+    
+    thrusterSignalSeq.add(ampE.getOutput())
 
-            # store input intcode program
-            inputFile = open("../tmp.txt", fmRead)
-            program = inputFile.readAll()
-            close(inputFile)
-            for code in program.split(","):
-                programSeq.add(parseInt(code))
-            
-            # run program
-            var i: int = 0
-            var pointerShift: int = 0
-            while getInstCode(programSeq[i]) != 99:
-                case getInstCode(programSeq[i])
-                of 1:
-                    opeCode1(i, programSeq)
-                    pointerShift = 4
-                of 2:
-                    opeCode2(i, programSeq)
-                    pointerShift = 4
-                of 3:
-                    if ( phaseSettingDone[amp] == false ):
-                        var phase: int = phaseSetingTable[amp]
-                        opeCode3(i, programSeq, phase)
-                        phaseSettingDone[amp] = true
-                    else:
-                        opeCode3(i, programSeq, input)
-                    pointerShift = 2
-                of 4:
-                    output = opeCode4(i, programSeq)
-                    pointerShift = 2
-                of 5:
-                    opeCode5(i, programSeq, pointerShift)
-                of 6:
-                    opeCode6(i, programSeq, pointerShift)
-                of 7:
-                    opeCode7(i, programSeq)
-                    pointerShift = 4
-                of 8:
-                    opeCode8(i, programSeq)
-                    pointerShift = 4
-                else:
-                    echo "ERR: Invalid opecode ", i, " ", programSeq[i]
-                    break
-                inc(i, pointerShift)
-                echo programSeq
+echo "Highest Signal is ", thrusterSignalSeq.max()
 
-    thrusterSignalSeq.add(output)
-
-echo "The highest thruster signal: ", thrusterSignalSeq.max
